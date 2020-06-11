55f8e049f52fa1226699fe7a9e44eaae08a9186e
Amber Cok <ambercok@macbookmber4737.home>
2020-06-11 11:14:05 +0200

    add greedy algorithm

--- codes/greedy_forward.py
+++ codes/greedy_forward.py
@@ -1,14 +1,8 @@
-from .connections import City
-from .connections import Connections
+from connections import Connections
 import random
 import csv
 
-# kopie van random.py file om algoritme van hill climber te testen
-
-# idee is dat het random algoritme steeds random een buurstad kiest.
-# het algoritme stopt wanneer het alle verbindingen heeft gehad.
-
-class Hillclimb(object):
+class Greedy_forward(object):
     def __init__(self, connections_object):
         self.cities = connections_object.cities
         self.ids = connections_object.city_ids
@@ -18,103 +12,98 @@ class Hillclimb(object):
         self.max_trajects = 7
         self.max_time = 120
         self.total_time = 0
-
-
+        
+        print(self.all_connections)
+     
     # dit is het random algoritme dat de trajecten probeert te vinden.
-    def find_traject(self):
+    def find_traject(self, steps_forward):
         for i in range(self.max_trajects):
             start = random.randint(0, len(self.cities) - 1)
             start_city = self.cities[start]
             time = 0
             # wordt gebruikt om te checken of het traject niet over tijdslimiet gaat.
             under_timelimit = True
-
             traject = [start_city.name]
-
+            
+            print("start:", start_city.name, time, traject)
+            
             while under_timelimit:
-
                 # stopt het find_traject algoritme wanneer alle verbindingen al zijn gemaakt
                 if len(self.used_connections) == len(self.all_connections):
                     return self.output()
-
+                
                 # checkt welke neighbours er zijn en of ze niet dubbel gebruikt zullen worden.
                 neighbours = start_city.get_neighbours()
                 neighbours = self.check_traject(start_city, neighbours)
+                # random buur kiezen
+                greedy_val = self.choose_shortest_time(start_city, neighbours)
                 
-                # kies een buur aan de hand van welke verbinding het kortst is
-                next_city = self.get_city(start_city, neighbours)
+                #random_val = random.randint(0, len(neighbours) - 1)
+                next_city = neighbours[greedy_val]
+                traject.append(next_city.name)
                 next_time = start_city.get_time(next_city)
-
+                
+                print("greedy:", start_city.name, next_city.name, next_time)
+                
                 # begint nieuw traject als het tijdslimiet overschreden is.
                 if time + next_time >= self.max_time:
                     under_timelimit = False
                     break
 
-                traject.append(next_city.name)
                 time += next_time
-
+                
                 # nieuwe stad wordt toegevoegd aan used_connections
                 city_pair = [start_city.name, next_city.name]
                 city_pair.sort()
                 if city_pair not in self.used_connections:
                     self.used_connections.append(city_pair)
-
+                else:
+                    print("double")
+                
                 start_city = next_city
 
-            # Remove the quotes from the traject list
-            final_traject = "[%s]" % (', '.join(traject))
-            self.trajects.append(final_traject)
+            self.trajects.append(traject)
             self.total_time += time
-
+            
         return self.output()
-
-    # Kiest de stad die het dichts bij ligt door alle mogelijke buren te checken
-    def get_city(self, city, neighbours):
-        # Hill climber
-        # Kies random state
-        random_val = random.randint(0, len(neighbours) - 1)
-        next_city = neighbours[random_val]
-        time = city.get_time(next_city)
-
-        # Doe beste van alle mogelijke verbindingen
-        for neighbour in neighbours:
-            check_city = neighbour.name
-            check_time = city.get_time(neighbour)
-
-            # Check of die verbinding al is gemaakt
-            city_pair = [city.name, check_city]
-            city_pair.sort()
-
-            if city_pair not in self.used_connections:
-                # Verander de stad en tijd als de verbinding voordeliger is
-                if check_time < time:
-                    next_city = neighbour
-                    time = check_time
-
-        return next_city
-
+        
+    
     # checkt of de buren niet al eerder zijn gebruikt.
     def check_traject(self, city, neighbours):
         new_neigh = []
-
         for neighbour in neighbours:
             city_pair = [city.name, neighbour.name]
             city_pair.sort()
             if city_pair not in self.used_connections:
                 new_neigh.append(neighbour)
-
+        
         # geeft de alle buren mee indien alle buren als eens zijn gebruikt.
         if new_neigh == []:
+            print("all neighbours are already used")
             return neighbours
         return new_neigh
+    
+    def choose_shortest_time(self, city, neighbours):
+        shortest_time_id = 0
+        identity = 0
+        prev_neigh = neighbours[0]
 
+        for neighbour in neighbours:
+            if city.get_time(neighbour) < city.get_time(prev_neigh):
+                print("new time:", city.name, neighbour.name, city.get_time(neighbour))
+                shortest_time_id = identity
+            identity += 1
+
+        return shortest_time_id
+        
+    
     # berekent de kwaliteit van de lijnvoering
     def score(self):
         p = len(self.used_connections) / len(self.all_connections)
-        print(len(self.used_connections), len(self.all_connections))
-
+        print(len(self.used_connections), len(self.all_connections), self.used_connections)
+        print(p, len(self.trajects), self.total_time)
         return p * 10000 - (len(self.trajects) * 100 + self.total_time)
-
+    
     # geeft de output voor in het csv bestand.
     def output(self):
         score = self.score()
@@ -124,12 +113,18 @@ class Hillclimb(object):
             string = "train_" + str(i)
             output.append([string, traject])
             print(["train_{}".format(i), traject])
-
         output.append(["score", score])
+        print(output)
         print("score", score)
+        
 
-        with open('results/toutput.csv', 'w', newline='') as file:
+        with open('output.csv', 'w', newline='') as file:
             writer = csv.writer(file, delimiter=',')
             writer.writerows(output)
         pass
+        
 
+if __name__ == "__main__":
+    # Run the connections class with the connections file
+    connections = Connections("data/ConnectiesHolland.csv")
+    Greedy_forward(connections).find_traject(2)
