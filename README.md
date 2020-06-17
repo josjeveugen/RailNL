4b64124c05243d4bc11be7e6820b2ffa987bc4df
Julia van Tol <Julia@MacBook-Pro-van-Julia.local>
2020-06-17 13:30:50 +0200

    Updated readme

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # RailNL
-UvA minor programmeren; programmeertheorie; case: RailNL
+Deze case gaat over het maken van een lijnvoering
+ voor intercitytreinen. Dat betekent dat je binnen een gegeven tijdsframe een aantal trajecten uitzet. 
+ Een traject is een route van sporen en stations waarover treinen heen en weer rijden. Een traject mag niet langer zijn dan het opgegeven tijdsframe.
 
-made by amber, josje, julia
+### Auteurs
+* Amber Cok
+* Josje Veugen
+* Julia van Tol
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from codes import connections
 from codes import random_algoritme as r
-from codes import test_hillclimber as hc
 from codes import greedy as g
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     # Run the connections class with the connections file
-    load_connections = connections.Connections("data/ConnectiesHolland.csv")
+    load_connections = connections.Connections("data/ConnectiesNationaal.csv")
 
     # Verwijder comment van het algoritme wat je wil runnen
 
@@ -17,7 +16,7 @@ if __name__ == "__main__":
     # hill_climber = hc.Hillclimb(load_connections).find_traject()
     
     # --------------------------- Run Greedy algoritme --------------------------
-    #greedy = g.Greedy(load_connections).find_traject()
+    greedy = g.Greedy(load_connections)
 
     
 def calculate_mean_score(x = 100):
